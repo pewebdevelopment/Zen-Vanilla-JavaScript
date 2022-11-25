2894ad7d5916629c88c044aecd73c9ef6ea297f9
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-25 22:22:26 +0530

    Navbar toggle project done

--- javascript-basic-projects-master/04-navbar/final/app.js
+++ javascript-basic-projects-master/04-navbar/final/app.js
@@ -1,8 +1,8 @@
 // classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+// classList.contains - checks classList for specific class
+// classList.add - add class
+// classList.remove - remove class
+// classList.toggle - toggles class
 
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
