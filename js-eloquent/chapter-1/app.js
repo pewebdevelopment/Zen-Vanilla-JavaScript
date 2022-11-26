bb4daaf52b45b6ade58fd3a43ab228f500685f4b
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-26 07:50:54 +0530

    updates

---
+++ js-eloquent/chapter-1/app.js
@@ -0,0 +1,19 @@
+// print
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+// in the console
+
+let str = "";
+for (i = 0; i < 7; i++) {
+  str += "#";
+  console.log(str + "\n");
+}
+
+console.log(str);
