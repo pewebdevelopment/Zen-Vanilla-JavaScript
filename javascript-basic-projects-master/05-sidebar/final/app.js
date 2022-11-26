306721f8d9f4851a9e4887b64f137b492c99405f
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-26 06:11:34 +0530

    sidebar project is done

--- javascript-basic-projects-master/05-sidebar/final/app.js
+++ javascript-basic-projects-master/05-sidebar/final/app.js
@@ -3,14 +3,22 @@ const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
 toggleBtn.addEventListener("click", function () {
-  // if (sidebar.classList.contains("show-sidebar")) {
-  //   sidebar.classList.remove("show-sidebar");
-  // } else {
-  //   sidebar.classList.add("show-sidebar");
-  // }
   sidebar.classList.toggle("show-sidebar");
 });
 
 closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
+  sidebar.classList.toggle("show-sidebar");
 });
+
+// toggleBtn.addEventListener("click", function () {
+//   // if (sidebar.classList.contains("show-sidebar")) {
+//   //   sidebar.classList.remove("show-sidebar");
+//   // } else {
+//   //   sidebar.classList.add("show-sidebar");
+//   // }
+//   sidebar.classList.toggle("show-sidebar");
+// });
+
+// closeBtn.addEventListener("click", function () {
+//   sidebar.classList.remove("show-sidebar");
+// });
